ad4cd3c750873bf885d3953d09df90630e595d48
arthur <sousanot.23@gmail.om>
2021-07-20 15:44:54 -0300

    criando tabela do course

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,12 +1,37 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Course } from './course';
 
 @Component({
     selector: 'app-course-list',
     templateUrl: './course-list.component.html'
 })
-export class CourseListComponent {
+export class CourseListComponent implements OnInit{
 
     courses: Course[] = [];
+
+    ngOnInit(): void{
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8796',
+                duration: 120,
+                rating: 4.5,
+                releaseDate: 'December, 2, 2019'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-8796',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'Octuber, 2, 2020'
+            }
+        ]
+    }
 
 }
