a35dfb6a657dde893c871f4f792369646b7ad2c8
arthur <sousanot.23@gmail.om>
2021-07-21 13:48:43 -0300

    arquivos modificados

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -14,7 +14,7 @@ export class CourseListComponent implements OnInit{
             {
                 id: 1,
                 name: 'Angular: Forms',
-                imageUrl: '',
+                imageUrl: '/assets/images/forms.png',
                 price: 99.99,
                 code: 'XPS-8796',
                 duration: 120,
@@ -24,7 +24,7 @@ export class CourseListComponent implements OnInit{
             {
                 id: 2,
                 name: 'Angular: HTTP',
-                imageUrl: '',
+                imageUrl: '/assets/images/http.png',
                 price: 45.99,
                 code: 'LKL-8796',
                 duration: 80,
