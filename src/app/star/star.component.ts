fd05348880c046bcdc2cd22c432fe7deebb937fb
arthur <sousanot.23@gmail.om>
2021-07-21 14:22:01 -0300

    adicionando modificações

--- src/app/star/star.component.ts
+++ src/app/star/star.component.ts
@@ -1,8 +1,20 @@
-import { Component } from "@angular/core";
+import { Component, Input, OnChanges } from "@angular/core";
 
 @Component({
-    selector: 'app-star'
+    selector: 'app-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
 })
-export class StarComponent {
+export class StarComponent implements OnChanges{
+
+    @Input()
+    rating: number = 0;
+
+    starWidth!: number;
+
+
+    ngOnChanges(): void{
+        this.starWidth = this.rating * 94 / 5;
+    }
     
 }
