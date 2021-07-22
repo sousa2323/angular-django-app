92087270b9899561d2f311c1db817eacffe83d6f
arthur <sousanot.23@gmail.om>
2021-07-21 21:16:56 -0300

    adicionando os demais componentes  do projeto

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { Course } from './course';
+import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
@@ -8,30 +9,12 @@ import { Course } from './course';
 export class CourseListComponent implements OnInit{
 
     courses: Course[] = [];
+    
+    constructor(private courseService: CourseService) { }
 
     ngOnInit(): void{
-        this.courses = [
-            {
-                id: 1,
-                name: 'Angular: Forms',
-                imageUrl: '/assets/images/forms.png',
-                price: 99.99,
-                code: 'XPS-8796',
-                duration: 120,
-                rating: 4.5,
-                releaseDate: 'December, 2, 2019'
-            },
-            {
-                id: 2,
-                name: 'Angular: HTTP',
-                imageUrl: '/assets/images/http.png',
-                price: 45.99,
-                code: 'LKL-8796',
-                duration: 80,
-                rating: 4,
-                releaseDate: 'October, 2, 2020'
-            }
-        ]
+        this.courses = this.courseService.retrieveAll();
+        
     }
 
 }
