6ddec9c225b6c2e208a68d8ce5950b334a2b6edb
arthur <sousanot.23@gmail.om>
2021-07-27 14:54:24 -0300

    adicionando pasta e arquivo de pipe

---
+++ webs/src/app/pipe/replace.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from "@angular/core";
+
+@Pipe({
+    name: 'replace'
+})
+export class ReplacePipe implements PipeTransform{
+
+        transform(value: string, char: string, valueToReplace: string) {
+            return valueToReplace.replace(char, valueToReplace);
+        }
+}
